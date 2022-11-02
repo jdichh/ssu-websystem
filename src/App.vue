ad7ada625885301969fe324bab81bbd62b27b3cc
jdichh <jpsdichoso21@gmail.com>
2022-11-02 21:48:10 +0800

    8th commit, low res/mobile detection

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,32 @@ import { sidebarWidth } from './components/sidebar/state'
 
 
 export default {
-  components: {Sidebar},
+
+  data() {
+    return {
+      mobile: null,
+    }
+  },
+
+  components: {
+    Sidebar
+  },
+
+  created () {
+    this.checkScreen();
+    window.addEventListener("resize", this.checkScreen)
+  },
+
+  methods: {
+    checkScreen() {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 850) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+    }
+  },
 
   setup() {
     const store = useStore()
@@ -43,6 +68,20 @@ body{
   color: black;
 }
 
+.mobile-message {
+  padding-top: 50%;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #434A52;
+  color: #fff;
+
+  p {
+    margin-top: 16px;
+  }
+}
+
 #nav {
   padding: 30px;
 }
@@ -58,7 +97,14 @@ body{
 </style>
 
 <template>
-  <Sidebar/>
-  <div :style="{ 'margin-left': sidebarWidth}"></div>
-  <router-view/>
+  <div v-if="!mobile">
+    <Sidebar/>
+      <div :style="{ 'margin-left': sidebarWidth}">
+      </div>
+    <router-view/>
+  </div>
+  <div v-else class="mobile-message flex flex-column">
+    <h2>Sorry, this cannot be viewed on mobile devices or low resolution monitors.</h2>
+    <p>To use this, please use a device that has a high resolution display.</p>
+  </div>
 </template>
